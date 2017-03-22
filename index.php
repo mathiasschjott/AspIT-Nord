06e77b41c13578fa3c4815610782aad2cb610627
DELL-SCHJOTT\schjo <mathias@schjott.dk>
2017-03-22 14:45:13 +0100

    Changes

--- index.php
+++ index.php
@@ -4,26 +4,86 @@
 	<meta charset="UTF-8">
 	<title>Dansk Netparty</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 	<header>
-		<img src="" alt="">
-		<nav>
-			<ul></ul>
-			<ul></ul>
-		</nav>	
+		<div class="container">
+			<img src="" alt="">
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="">Dansk Netparty</a>
+					</div>
+					<ul class="nav navbar-nav">
+						<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Information <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Generelt</a></li>
+								<li><a href="#">Information til forældre</a></li>
+								<li><a href="#">Regler</a></li>
+								<li><a href="#">Madplan</a></li>
+								<li><a href="#">Huskeliste</a></li>
+								<li><a href="#">Tidsplan</a></li>
+								<li><a href="#">Vilkår & betingelser</a></li>
+								<li><a href="#">Udlejning</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Turneringer</a></li>
+						<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Crew <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Holdet bag</a></li>
+								<li><a href="#">Foreningen</a></li>
+								<li><a href="#">Bliv crew</a></li>
+							</ul>
+						</li>
+						<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Galleri <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Dansk Netparty #7</a></li>
+								<li><a href="#">Dansk Netparty #6</a></li>
+								<li><a href="#">Dansk Netparty #5</a></li>
+								<li><a href="#">Dansk Netparty #4</a></li>
+								<li><a href="#">Dansk Netparty #3</a></li>
+								<li><a href="#">Dansk Netparty #2</a></li>
+								<li><a href="#">Dansk Netparty #1</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Kontakt</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Tilmeld</a></li>
+						<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log ind</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	</header>	
-	<div id="slider">
-		
+	<div id="slider" class="text-center">
+		<h1>Meget flot titel</h1>
+		<h3>Køb for satan, køb!</h3>
+		<a href="#" class="btn btn-info" role="button">Tilmeld dig</a>
 	</div>
 	<div id="sponsor">
 		
 	</div>
+	<div id="?">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+
+				</div>
+				<div class="col-sm-4">
+
+				</div>
+				<div class="col-sm-4">
+
+				</div>
+			</div>
+		</div>
+	</div>
 	<main>
 		
 	</main>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
 </body>
 </html>
